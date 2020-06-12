1dfb8dd8a5300314b8b0ff02e401b8eecf3904cd
Bilel <sparkismad@gmail.com>
2020-06-12 10:35:37 +0200

    adding files

---
+++ Zoo_Nevot/src/ZooNeuvot/Animal.java
@@ -0,0 +1,11 @@
+package ZooNeuvot;
+
+public class Animal 
+{
+	private String name;		// Nom de l'espèce
+	private String sex;			// Sexe "Male" ou "Female"
+	private double weight;		// Poids en Kg
+	private double size;		// Taille en cm
+	private int age;			// Age de l'animal
+	
+}
